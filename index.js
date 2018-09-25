84959ccf7cd6f68a0877d8759906106b9c1e3646
Alvin Milton <al@thehandiest.com>
2018-09-24 20:18:58 -0400

    parsing method and logging it

--- index.js
+++ index.js
@@ -15,11 +15,16 @@ var server = http.createServer(function(req, res) {
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
+  // get the http method
+  var method = req.method.toLowerCase();
+
   //   send response
   res.end("hello world\n");
 
   //   log the request
-  console.log("Request recieved on path: " + trimmedPath);
+  console.log(
+    "Request recieved on path: " + trimmedPath + " with method " + method
+  );
 });
 
 // start the server and listen on 3000
