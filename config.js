a1fa6b8d9bb758d81a515237b838f22b3a85ba6b
Alvin Milton <al@thehandiest.com>
2018-09-24 21:12:02 -0400

    config file and objects

---
+++ config.js
@@ -0,0 +1,10 @@
+// create and export config vars
+
+// container for all environments
+var environments = {};
+
+// staging obj
+environments.staging = {};
+
+// production obj
+environments.production = {};
