0a7d8c43549e8238b08b1c479d4b9a00537cd0d1
Alvin Milton <al@thehandiest.com>
2018-09-24 21:18:30 -0400

    store command line arg as var and test and default to value

--- config.js
+++ config.js
@@ -4,7 +4,25 @@
 var environments = {};
 
 // staging obj
-environments.staging = {};
+environments.staging = {
+  port: 5000,
+  envName: "staging"
+};
 
 // production obj
-environments.production = {};
+environments.production = {
+  port: 6000,
+  envName: "production"
+};
+
+// determine which environment was posted as a command line arg
+var currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "";
+
+// check that currentEnv is set to a valid key: staging or production
+var envToExport =
+  typeof environments[currentEnvironment] == "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
