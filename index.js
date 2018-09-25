1c16b20310da49a05701d07cab3dec7eb7ee1bd4
Alvin Milton <al@thehandiest.com>
2018-09-24 20:15:48 -0400

    parsing the request

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ Primary file for API
 
 // dependencies
 var http = require("http");
+var url = require("url");
 
 // the server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
+  //   get url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  //   get path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  //   send response
   res.end("hello world\n");
+
+  //   log the request
+  console.log("Request recieved on path: " + trimmedPath);
 });
 
 // start the server and listen on 3000
